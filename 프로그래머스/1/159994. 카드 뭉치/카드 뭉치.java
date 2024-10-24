e773873e1526860be8f11e5079af7e4d8371ee9c
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2024-10-24 16:07:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.26 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> queue1 = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> queue2 = new LinkedList<>(Arrays.asList(cards2));
+        
+        for(int i = 0; i < goal.length; i++) {
+            if(!queue1.isEmpty() && goal[i].equals(queue1.peek())) {
+                queue1.poll();
+            } else if (!queue2.isEmpty() && goal[i].equals(queue2.peek())) {
+                queue2.poll();
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+        
+    }
+}
