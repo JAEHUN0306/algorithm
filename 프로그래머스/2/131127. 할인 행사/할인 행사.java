3f627b6d73b10c39b9434ea8e0efd4849dd106db
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2025-03-20 18:47:11 +0900

    [level 2] Title: 할인 행사, Time: 72.00 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,37 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        // 원하는 제품과 수량을 맵에 저장
+        int count = 0;
         Map<String, Integer> wantMap = new HashMap<>();
         for(int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
         
-        int answer = 0;
+        Map<String, Integer> windowMap = new HashMap<>();
+        for(int i = 0; i < 10; i++) {
+            windowMap.put(discount[i], windowMap.getOrDefault(discount[i], 0) + 1);
+        }
+        if(isValid(wantMap, windowMap)) count++;
         
-        // 슬라이딩 윈도우 사용
-        for(int i = 0; i <= discount.length - 10; i++) {
-            // 현재 윈도우의 제품 수량을 맵에 저장
-            Map<String, Integer> windowMap = new HashMap<>();
-            for(int j = 0; j < 10; j++) {
-                String item = discount[i + j];
-                windowMap.put(item, windowMap.getOrDefault(item, 0) + 1);
-            }
-            
-            // 원하는 제품과 수량이 모두 일치하는지 확인
-            boolean isValid = true;
-            for(String item : wantMap.keySet()) {
-                if(windowMap.getOrDefault(item, 0) < wantMap.get(item)) {
-                    isValid = false;
-                    break;
-                }
-            }
-            
-            if(isValid) {
-                answer++;
+        for(int i = 10; i < discount.length; i++) {
+            windowMap.put(discount[i], windowMap.getOrDefault(discount[i], 0) + 1);
+            windowMap.put(discount[i - 10], windowMap.getOrDefault(discount[i - 10], 0) - 1);
+            if(isValid(wantMap, windowMap)) count++;
+        }
+        return count;
+    }
+    
+    public boolean isValid(Map<String, Integer> map1, Map<String, Integer> map2) {
+        for(String s : map1.keySet()) {
+            if(map2.getOrDefault(s, 0) < map1.get(s)) {
+                return false;
             }
         }
-        
-        return answer;
+        return true;
     }
 }
