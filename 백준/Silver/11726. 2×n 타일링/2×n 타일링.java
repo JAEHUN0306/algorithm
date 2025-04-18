9e5397ab5e503930c82ee5457fb108bc597e019e
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2025-04-18 15:50:32 +0900

    [Silver III] Title: 2×n 타일링, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        // n이 1이나 2인 경우 바로 결과 반환
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        if (n == 2) {
+            System.out.println(2);
+            return;
+        }
+        
+        // 2 x n 타일을 1 x 2 , 2 x 1타일로 채우는 경우의 수
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
