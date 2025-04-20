40ac881036b2b93b676b3f10add5be7d489abd2b
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2025-04-20 18:52:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,28 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+
         String[] split = s.split("-");
 
+        String[] start = split[0].split("\\+");
         int result = 0;
-
-        String[] first = split[0].split("\\+");
-        for (String str : first) {
-            if (!str.isEmpty()) {
-                result += Integer.parseInt(str);
+        for (String str : start) {
+            if(!str.isEmpty()) { // '-'로 시작하면 빈 문자열
+            result += Integer.parseInt(str);
             }
         }
 
         for (int i = 1; i < split.length; i++) {
-            String[] add = split[i].split("\\+");
+            String[] addPart = split[i].split("\\+");
             int sum = 0;
-            for (String str : add) {
-                if (!str.isEmpty()) {
-                    sum += Integer.parseInt(str);
-                }
+            for (String add : addPart) {
+                sum += Integer.parseInt(add);
             }
             result -= sum;
         }
-
         System.out.println(result);
     }
 }
