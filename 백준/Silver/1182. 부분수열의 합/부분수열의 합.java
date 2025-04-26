ab23fefcabbad4354311d94ff2afb267610eb002
JAEHUN0306 <111242302+JAEHUN0306@users.noreply.github.com>
2025-04-26 17:56:48 +0900

    [Silver II] Title: 부분수열의 합, Time: 176 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
+    static int n, t, result;
     static int[] nums;
-    static int[] ch;
-    static int n, s;
-    static int count;
     public static void main(String[] args) {
+        /*
+        N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하는 프로그램을 작성하시오.
+         */
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        s = sc.nextInt();
+        t = sc.nextInt();
 
         nums = new int[n];
-        ch = new int[n];
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
 
         DFS(0, 0, 0);
-        System.out.println(count);
+        System.out.println(result);
     }
 
-    static void DFS(int L, int sum, int element) {
-        if(L ==  n) {// 부분 수열이 완성 됐으면
-            if (sum == s && element != 0) count++;
+    public static void DFS(int L, int sum, int elements) {
+        if(L == n) {
+            if (elements > 0 && sum == t) result++;
         } else {
-            DFS(L + 1, sum + nums[L], element + 1);
-            DFS(L + 1, sum, element);
+            DFS(L + 1, sum + nums[L], elements + 1);
+            DFS(L + 1, sum, elements);
         }
     }
 }
